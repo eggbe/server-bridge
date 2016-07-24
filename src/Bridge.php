c49cd249378e0be95cdb7b7c4e3685dec0b38960
hacpaka <hacpaka@eggbe.com>
2016-07-25 01:36:42 +0300

    +

--- src/Bridge.php
+++ src/Bridge.php
@@ -30,67 +30,55 @@ class Bridge {
 	 */
 	public function dispatch(array $Request) {
 		$Request = array_change_key_case($Request, CASE_LOWER);
-		try {
 
-			foreach ($this->Callbacks as $Rules => $Calbacks) {
+		foreach ($this->Callbacks as $Rules => $Calbacks) {
+
+			/**
+			 * All positive conditions are parsed here.
+			 *
+			 * This type of conditions don't need any special character in the beginning
+			 * and always checked in the first place.
+			 */
+			$Keys = array_filter($Rules = explode(',', $Rules), function ($key) {
+				return !preg_match('/^[:!]/', $key);
+			});
+			if (count(array_diff($Keys, array_keys($Request))) < 1) {
 
 				/**
-				 * All positive conditions are parsed here.
+				 * All negative conditions are parsed here.
 				 *
-				 * This type of conditions don't need any special character in the beginning
-				 * and always checked in the first place.
+				 * This type of conditions have to start from a special negation character
+				 * and always checked in the second place.
 				 */
-				$Keys = array_filter($Rules = explode(',', $Rules), function ($key) {
-					return !preg_match('/^[:!]/', $key);
-				});
-				if (count(array_diff($Keys, array_keys($Request))) < 1) {
+				$Keys = array_map(function ($key) { return preg_replace('/^!/', null, $key); },
+					array_filter($Rules, function ($key) { return preg_match('/^!/', $key); }));
+				if (empty($Keys) || count(array_diff($Keys, array_keys($Request))) == count($Keys)) {
 
 					/**
-					 * All negative conditions are parsed here.
+					 * All grabbable keys are parsed here.
 					 *
-					 * This type of conditions have to start from a special negation character
-					 * and always checked in the second place.
+					 * This list are always includes all positive conditions and conditions
+					 * started by special colon character.
 					 */
-					$Keys = array_map(function ($key) { return preg_replace('/^!/', null, $key); },
-						array_filter($Rules, function ($key) { return preg_match('/^!/', $key); }));
-					if (empty($Keys) || count(array_diff($Keys, array_keys($Request))) == count($Keys)) {
+					$Request = Arr::like($Request, array_map(function ($key) { return preg_replace('/^:/', null, $key); },
+						array_filter($Rules, function ($key) { return !preg_match('/^!/', $key); })));
 
-						/**
-						 * All grabbable keys are parsed here.
-						 *
-						 * This list are always includes all positive conditions and conditions
-						 * started by special colon character.
-						 */
-						$Request = Arr::like($Request, array_map(function ($key) { return preg_replace('/^:/', null, $key); },
-							array_filter($Rules, function ($key) { return !preg_match('/^!/', $key); })));
-
-						foreach ($Calbacks as $Callback) {
-							call_user_func_array($Callback, $Request);
-
-//							try {
-//
-//							} catch (\Exception $Exception) {
-//
-//							}
-//							_dumpe($Callback);
-
-//							if (!is_null(($Response = call_user_func_array($Callback, Arr::like($Request, $Keys))))) {
-//								return json_encode(['error' => false,
-//									'data' => $Response]);
-//							}
+					foreach ($Calbacks as $Callback) {
+						if (!is_null(($response = call_user_func_array($Callback, $Request)))) {
+							return $response;
 						}
-
 					}
-				}
 
+				}
 			}
-		} catch (\Exception $Exception) {
-			return json_encode(['error' => true,
-				'message' => $Exception->getMessage()]);
+
 		}
 
-		return json_encode(['error' => false,
-			'data' => null]);
+		/**
+		 * If none of callbacks returned any non-empty value
+		 * then boolean false will be returned by default.
+		 */
+		return false;
 	}
 
 }
